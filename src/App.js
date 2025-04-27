d14e6d338b737ac9d7c533f97b00ae70865c9b2e
etaryu <yuggoteruya1@gmail.com>
2025-04-27 11:34:50 -0400

    Rota da compra

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home';
+import Compras from './pages/Compras';
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/compras" element={<Compras />} />
     </Routes>
   </Router>
 );
